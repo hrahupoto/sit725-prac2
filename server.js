0e85d67f9d4c5aec8275da1e6ec9d3a9eee88966
Hassan Abbas Rahupoto <hrahupoto@gmail.com>
2020-07-25 16:18:37 +1000

    Getting user details from an array works

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express')
+const express = require('express');
+const { isString } = require('util');
 const app = express()
-var result = 0, num1 = 0, num2 = 0;
+var result = 0, num1 = 0, num2 = 0, user = '', string = 'User not found';
 
 
 app.use(express.static(__dirname + '/public'))
@@ -30,7 +31,25 @@ app.get('/add_operator', function(request, response){
     response.send('The sum is '+ sum)     
 })
 
+let accounts = [
+    {id:1, name: 'alex', deposit: 5},
+    {id:2, name: 'sarah', deposit: 5},
+    {id:3, name: 'jim', deposit: 15}
+] 
 
+//Filter's user account by name
+app.get('/user_account', function(request,response){
+    user = request.query.user;
+    let data = accounts.filter(function(account){
+        if(account.name === user){
+            return account.name;
+        }
+        
+    });
+
+    console.log(JSON.stringify(data));
+    response.send('The user details are '+ JSON.stringify(data))
+})
 
 //start server and listen on port 3000
 app.listen(3000)
